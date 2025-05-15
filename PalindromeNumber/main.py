3a67c047ab5e4335a04b5f87f799dbc5fa11d71a
Hải Tân <221124022310@due.udn.vn>
2025-05-15 17:10:54 +0700

    Hai commit v3

--- PalindromeNumber/main.py
+++ PalindromeNumber/main.py
@@ -1,5 +1,5 @@
-class Solution(object):
-  def is_palindrome(self, x):
+class Solution:
+  def is_palindrome(self, x: int) -> bool:
     if x < 0 or (x % 10 == 0 and x != 0):
       return False
     
