0861c606c095ecedd0f532b78d83fc186d0531bd
Hải Tân <221124022310@due.udn.vn>
2025-05-21 11:39:51 +0700

    Hai commit v16

---
+++ 27-RemoveElement/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+
+	return i
+}
+
+func main() {
+	fmt.Println(removeElement([]int{0,1,2,2,3,0,4,2}, 2))
+}
