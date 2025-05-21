47285e8b093fdee8aea524e4211c0cf9b09da7a2
Hải Tân <221124022310@due.udn.vn>
2025-05-21 15:10:00 +0700

    Hai commit v19

--- 27-RemoveElement/Main.java
+++ 27-RemoveElement/Main.java
@@ -18,6 +18,6 @@ class RemoveElement {
   public static void main(String[] args) {
     RemoveElement sol = new RemoveElement();
     System.out.println(sol.removeElement(new int[] {3, 2, 2, 3}, 3));
-    System.out.println(sol.removeElement(new int[] {0, 1, 2, 2, 3, 0, 4, 2}, 2))
+    System.out.println(sol.removeElement(new int[] {0, 1, 2, 2, 3, 0, 4, 2}, 2));
   }
 }
