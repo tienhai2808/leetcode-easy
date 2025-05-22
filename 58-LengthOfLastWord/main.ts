27efae95c0efe89a2dec13abdfa39854f0c50dce
Hải Tân <221124022310@due.udn.vn>
2025-05-22 20:23:27 +0700

    Hai commit v23

---
+++ 58-LengthOfLastWord/main.ts
@@ -0,0 +1,19 @@
+const lengthOfLastWord = (s: string): number => {
+  let length: number = 0;
+  let i: number = s.length - 1;
+
+  while (i >= 0 && s[i] === " ") {
+    i--;
+  }
+
+  while (i >= 0 && s[i] !== " ") {
+    length++;
+    i--;
+  }
+
+  return length;
+}
+
+console.log(lengthOfLastWord("Hello World"));
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(lengthOfLastWord("luffy is still joyboy"));
